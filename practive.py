c894976ff4994e3fdca48dcaff61117006abfe8f
Nguyễn Minh Đức <duc2820@gmail.com>
2019-08-09 00:58:50 +0700

    all practive python

---
+++ practive.py
@@ -0,0 +1,16 @@
+class dog:
+    data = 0
+
+    def __init__(self):
+        self.data =  1
+    def test(self):
+       print [1,2,3,4]
+        # print (type(("nguyen minh duc",self.data))
+    def firstFunc(self):
+        self.test()
+        print self.data
+x =  dog()
+# x.test()
+x.firstFunc()
+
+
